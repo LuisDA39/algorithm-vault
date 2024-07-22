495c35400f89a980b81503319a0e777a92bec0b6
Luis De Anda <107770749+LuisDA39@users.noreply.github.com>
2024-07-22 15:56:27 -0600

    Update trie.py

--- trees/trie.py
+++ trees/trie.py
@@ -1,1 +1,47 @@
+class Node:
+    def __init__(self):
+        self.children = [None] * 26
+        self.is_leaf = False
 
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def char_to_idx(self, c):
+        return ord(c) - ord('a')
+
+    def insert(self, word):
+        curr_node = self.root
+
+        for c in word:
+            idx = self.char_to_idx(c)
+            if curr_node.children[idx] is None:
+                curr_node.children[idx] = Node()
+
+            curr_node = curr_node.children[idx]
+
+        curr_node.is_leaf = True
+
+    def search(self, word):
+        curr_node = self.root
+
+        for c in word:
+            idx = self.char_to_idx(c)
+
+            if curr_node.children[idx] is None:
+                return False
+
+            curr_node = curr_node.children[idx]
+
+        return curr_node.is_leaf
+
+
+# Example
+trie = Trie()
+trie.insert('algorithms')
+trie.insert('algo')
+trie.insert('hi')
+print(trie.search('algorithms'))
+print(trie.search('algo'))
+print(trie.search('hello'))
